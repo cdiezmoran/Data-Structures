711e4070dda79bf7d5dcf7259d3bf5943d7ae10a
Carlos Diez <cdiezmoran@gmail.com>
2017-03-27 10:55:19 -0700

    Finish palindromes iterative all 7 passing

--- source/palindromes.py
+++ source/palindromes.py
@@ -2,6 +2,7 @@
 
 # Hint: use string.ascii_letters (all letters in ASCII character set)
 import string
+import re
 
 
 def is_palindrome(text):
@@ -13,12 +14,46 @@ def is_palindrome(text):
     return is_palindrome_iterative(text)
     # return is_palindrome_recursive(text)
 
-
+# This function looks for pairs of letters and if those pairs count are equal
+# to the length of the string / 2 then it is a palindrome and returns true
 def is_palindrome_iterative(text):
-    # TODO: implement the is_palindrome function iteratively here
-    pass
-    # once implemented, change is_palindrome to call is_palindrome_iterative
-    # to verify that your iterative implementation passes all tests
+    # Ignore caps
+    text = text.lower()
+    # Remove spaces and special characters
+    text = re.sub('[^A-Za-z0-9]+', '', text)
+
+    unique_letters = {}
+    for letter in text:
+        if letter in unique_letters:
+            # If letter has a pair already
+            if unique_letters[letter] == 2:
+                count = 1
+                entry = letter + str(count)
+                # Look for the next available pair or create one (i.e. for a then "a1", "a2" ...)
+                while entry in unique_letters and unique_letters[entry] == 2:
+                    count += 1
+                    entry = letter + str(count)
+                # Next entry is in unique_letters but does not have a pair
+                if entry in unique_letters:
+                    unique_letters[entry] += 1
+                # Next entry is not in unique_letters
+                else:
+                    unique_letters[entry] = 1
+            # Letter is already in unique_letters but does not have a pair
+            else:
+                unique_letters[letter] += 1
+        # Letter is not in unique_letters
+        else:
+            unique_letters[letter] = 1
+
+    half_len = len(text) / 2
+
+    # If text len is even then it must be equal
+    if len(text) % 2 == 0:
+        return half_len == len(unique_letters)
+
+    # If text len is odd then we have to add 1 to it and compare (round up)
+    return (half_len + 1) == len(unique_letters)
 
 
 def is_palindrome_recursive(text, left=None, right=None):
