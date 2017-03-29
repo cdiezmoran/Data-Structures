f84d8b93acd19d7860e0c0ec3588bc6efdb1dff6
Carlos Diez <cdiezmoran@gmail.com>
2017-03-29 11:11:56 -0700

    Finish string find_index

--- source/string_search.py
+++ source/string_search.py
@@ -16,12 +16,25 @@ def find(string, sub_string):
 
     return False
 
+def find_index(string, sub_string):
+    count = 0
+
+    for i, char in enumerate(string):
+        if char == sub_string[count]:
+            count += 1
+        else:
+            count = 0
+
+        if count == len(sub_string):
+            return i - (count - 1)
+
+    return None
 
 def main():
     string = sys.argv[1]
     sub_string = sys.argv[2]
 
-    print find(string, sub_string)
+    print find_index(string, sub_string)
 
 if __name__ == '__main__':
     main()
