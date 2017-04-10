2f9ee9d4b97834e1c9ffc60b7d80a064dc678fc2
Carlos Diez <cdiezmoran@gmail.com>
2017-04-10 13:51:35 -0700

    Finish traverse

--- source/binarysearchtree.py
+++ source/binarysearchtree.py
@@ -1,5 +1,7 @@
 #!python
 
+from queue import LinkedQueue
+
 class BinaryNode(object):
 
     def __init__(self, data):
@@ -15,20 +17,20 @@ class BinaryNode(object):
     def is_leaf(self):
         """Return True if this node is a leaf (has no children)"""
         # TODO: Check if both left child and right child have no value
-        return ... and ...
+        return self.left is None and self.right is None
 
     def is_internal(self):
         """Return True if this node is internal (has at least one child)"""
         # TODO: Check if either left child or right child has a value
-        return ... or ...
+        return self.left is not None or self.right is not None
 
     def height(self):
         """Return the number of edges on the longest downward path from this
         node to a descendant leaf node"""
         # TODO: Check if left child has a value and if so calculate its height
-        left_height = ... if self.left is not None else -1
+        left_height = self.left.height() if self.left is not None else -1
         # TODO: Check if right child has a value and if so calculate its height
-        right_height = ... if self.right is not None else -1
+        right_height = self.right.height() if self.right is not None else -1
         # Return one more than the greater of the left height and right height
         return 1 + max(left_height, right_height)
 
@@ -68,8 +70,8 @@ class BinarySearchTree(object):
         """Return an item in this binary search tree matching the given item"""
         # Find a node with the given item, if any
         node = self._find_node(item)
-        # TODO: Return the node's data if found, or None
-        return node.data if ... else None
+
+        return node.data if node is not None else None
 
     def _find_node(self, item):
         """Return the node containing the given item in this binary search tree,
@@ -78,18 +80,18 @@ class BinarySearchTree(object):
         node = self.root
         # Loop until we descend past the closest leaf node
         while node is not None:
-            # TODO: Check if the given item matches the node's data
-            if ...:
+            # Check if the given item matches the node's data
+            if item == node.data:
                 # Return the found node
                 return node
-            # TODO: Check if the given item is less than the node's data
-            elif ...:
-                # TODO: Descend to the node's left child
-                node = ...
-            # TODO: Check if the given item is greater than the node's data
-            elif ...:
-                # TODO: Descend to the node's right child
-                node = ...
+            # Check if the given item is less than the node's data
+            elif item < node.data:
+                # Descend to the node's left child
+                node = node.left
+            # Check if the given item is greater than the node's data
+            elif item > node.data:
+                # Descend to the node's right child
+                node = node.right
         # Not found
         return None
 
@@ -101,20 +103,20 @@ class BinarySearchTree(object):
         parent = None
         # Loop until we descend past the closest leaf node
         while node is not None:
-            # TODO: Check if the given item matches the node's data
-            if ...:
+            # Check if the given item matches the node's data
+            if item == node.data:
                 # Return the parent of the found node
                 return parent
-            # TODO: Check if the given item is less than the node's data
-            elif ...:
-                # TODO: Update the parent and descend to the node's left child
+            # Check if the given item is less than the node's data
+            elif item < node.data:
+                # Update the parent and descend to the node's left child
                 parent = node
-                node = ...
-            # TODO: Check if the given item is greater than the node's data
-            elif ...:
-                # TODO: Update the parent and descend to the node's right child
+                node = node.left
+            # Check if the given item is greater than the node's data
+            elif item > node.data:
+                # Update the parent and descend to the node's right child
                 parent = node
-                node = ...
+                node = node.right
         # Not found
         return parent
 
@@ -124,22 +126,22 @@ class BinarySearchTree(object):
         if self.is_empty():
         # if self.root is None:
             # TODO: Create a new root node
-            self.root = ...
+            self.root = BinaryNode(item)
             # TODO: Increase the tree size
-            self.size ...
+            self.size += 1
             return
         # Find the parent node of where the given item should be inserted
         parent = self._find_parent_node(item)
         # TODO: Check if the given item should be inserted left of the parent node
-        if ...:
+        if item < parent.data:
             # TODO: Create a new node and set the parent's left child
-            parent.left = ...
+            parent.left = BinaryNode(item)
         # TODO: Check if the given item should be inserted right of the parent node
-        elif ...:
+        elif item > parent.data:
             # TODO: Create a new node and set the parent's right child
-            parent.right = ...
+            parent.right = BinaryNode(item)
         # TODO: Increase the tree size
-        self.size ...
+        self.size += 1
 
     def items_in_order(self, node=None, items=None):
         """Return a list of all items in this binary search tree found using
@@ -149,12 +151,12 @@ class BinarySearchTree(object):
             node = self.root
         if items is None:
             items = list()
-        # TODO: Traverse left subtree, if it exists
-        ...
-        # TODO: Add this node's data to the items list
-        ...
-        # TODO: Traverse right subtree, if it exists
-        ...
+
+        if node.left is not None:
+            self.items_in_order(node.left, items)
+        items.append(node.data)
+        if node.right is not None:
+            self.items_in_order(node.right, items)
         # Return the items list to the original caller
         return items
 
@@ -166,12 +168,15 @@ class BinarySearchTree(object):
             node = self.root
         if items is None:
             items = list()
-        # TODO: Add this node's data to the items list
-        ...
-        # TODO: Traverse left subtree, if it exists
-        ...
-        # TODO: Traverse right subtree, if it exists
-        ...
+
+        if node is None:
+            return
+
+        items.append(node.data)
+        if node.left is not None:
+            self.items_pre_order(node.left, items)
+        if node.right is not None:
+            self.items_pre_order(node.right, items)
         # Return the items list to the original caller
         return items
 
@@ -183,12 +188,15 @@ class BinarySearchTree(object):
             node = self.root
         if items is None:
             items = list()
-        # TODO: Traverse left subtree, if it exists
-        ...
-        # TODO: Traverse right subtree, if it exists
-        ...
-        # TODO: Add this node's data to the items list
-        ...
+
+        if node is None:
+            return
+
+        if node.left is not None:
+            self.items_post_order(node.left, items)
+        if node.right is not None:
+            self.items_post_order(node.right, items)
+        items.append(node.data)
         # Return the items list to the original caller
         return items
 
@@ -196,24 +204,22 @@ class BinarySearchTree(object):
         """Return a list of all items in this binary search tree found using
         level-order traversal"""
         # TODO: Create a queue to store nodes not yet traversed in level-order
-        queue = ...
+        queue = LinkedQueue()
         # Create an items list
         items = list()
-        # TODO: Enqueue the root node if this tree is not empty
-        if ...:
-            queue...
-        # TODO: Loop until the queue is empty
-        while ...:
-            # TODO: Dequeue the node at the front of the queue
-            node = ...
-            # TODO: Add this node's data to the items list
-            ...
-            # TODO: Enqueue this node's left child if it exists
-            if ...:
-                ...
-            # TODO: Enqueue this node's right child if it exists
-            if ...:
-                ...
+
+        if not self.is_empty():
+            queue.enqueue(self.root)
+
+        while queue.is_empty() is False:
+            node = queue.dequeue()
+            items.append(node.data)
+
+            if node.left is not None:
+                queue.enqueue(node.left)
+
+            if node.right is not None:
+                queue.enqueue(node.right)
         # Return the items list
         return items
 
